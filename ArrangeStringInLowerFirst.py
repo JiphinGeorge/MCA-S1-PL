fbcbc40a9a4bfe5d8bdf1df24e182157780b5fe5
Jiphin George <160301570+JiphinGeorge@users.noreply.github.com>
2026-03-01 23:49:53 +0530

    Fix string joining in reorder_string function

--- ArrangeStringInLowerFirst.py
+++ ArrangeStringInLowerFirst.py
@@ -3,5 +3,4 @@ def reorder_string(s):
     lower = ''.join([c for c in s if c.islower()])
     upper = ''.join([c for c in s if c.isupper()])
     return lower + upper
-
 print(reorder_string("PyTHon"))
